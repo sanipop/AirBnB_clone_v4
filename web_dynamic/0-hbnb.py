4453847773e677b14569c9a80b6a1c9a836466cd
sanipop <67932273+sanipop@users.noreply.github.com>
2024-04-07 15:33:04 +0100

    Update 0-hbnb.py

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """
-Flask App that integrates with AirBnB static HTML Template
+integrate Flask App with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
 import uuid;
 
-# flask setup
+# setup flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
 
 
-# begin flask page rendering
+# render page
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
+    close sqlalchemy session
     """
     storage.close()
 
